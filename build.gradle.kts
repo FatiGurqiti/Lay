264e59c140c23aa02cc0281ee8e3fef300afb1c0
fatigurqiti <ho$28!ZT!83>
2022-07-31 15:43:52 +0200

    Gradle converted to Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath ("com.android.tools.build:gradle:7.2.1")
-        classpath ("com.google.gms:google-services:4.3.10")
+        classpath ("com.google.gms:google-services:4.3.13")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -32,7 +32,6 @@ extra.apply {
     set("appCompatVersion", "1.2.0")
     set("constraintLayoutVersion", "2.0.2")
     set("coreTestingVersion", "2.1.0")
-    set("lifecycleVersion", "2.2.0")
     set("materialVersion", "1.2.1")
     set("roomVersion", "2.4.0")
 // testing

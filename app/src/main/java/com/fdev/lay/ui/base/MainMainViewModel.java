ae43103cd348ddff03318fbb1c6976bd0c44635f
fatigurqiti <ho$28!ZT!83>
2022-08-25 22:51:06 +0200

    Sets username as live data

--- app/src/main/java/com/fdev/lay/ui/base/MainMainViewModel.java
+++ app/src/main/java/com/fdev/lay/ui/base/MainMainViewModel.java
@@ -2,10 +2,16 @@ package com.fdev.lay.ui.base;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class MainMainViewModel extends ViewModel {
 
     private MutableLiveData<String> username;
+
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public MutableLiveData<String> getUsername() {
         if (username == null) {
@@ -14,8 +20,18 @@ public class MainMainViewModel extends ViewModel {
         return username;
     }
 
-    public void setCurrentName(){
-        if (username != null)
-        username.setValue("username");
+    public void setCurrentName() {
+        if (username != null) {
+            db.collection("users")
+                    .whereEqualTo("email", mAuth.getCurrentUser().getEmail())
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                username.setValue((document.getString("username")));
+                            }
+                        }
+                    });
+        }
     }
 }
--- app/src/test/java/com/fdev/lay/MainLoggedInTest.java
+++ app/src/test/java/com/fdev/lay/MainLoggedInTest.java
@@ -4,26 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.fdev.lay.presentation.ui.main.HomePage;
 import com.fdev.lay.presentation.ui.search_fragment.SearchFragment;
-import com.fdev.lay.presentation.ui.favourite_list.FavouriteListFragment;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class MainLoggedInTest{
-
-
-    @Test
-    @Tag("Username")
-    public void ShouldGetTheUserNameCorrect() {
-        assertEquals(HomePage.getUsername(), FavouriteListFragment.getUsername());
-    }
-
     @Test
     @Tag("Quota")
     void ShouldGetQuotaCorrect() {
         assertEquals(HomePage.getQuota(), SearchFragment.getQuota());
     }
-
 
     @Test
     @Tag("TopRates")

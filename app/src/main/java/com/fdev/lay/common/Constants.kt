34af6f14f7245dbd57f207bf0a1d3e4fb64d5503
fatigurqiti <ho$28!ZT!83>
2022-08-25 22:03:00 +0200

    Main page rearrangement & kotlin implementation

--- app/src/main/java/com/fdev/lay/common/Constants.kt
+++ app/src/main/java/com/fdev/lay/common/Constants.kt
@@ -1,4 +1,23 @@
+@file:JvmName("ConstantUtils")
 package com.fdev.lay.common
 
+class LayConstant{
+
+    fun a(): String { return "asd"}
+
 object Constants {
+    var username = ""
+
+    var key = arrayListOf(
+        "faf5f15f11mshde808885082bbb3p1b4a6ejsn3384755128b8",
+        "1c94f2813amshf75073ced6bbcdep11f75ajsnf0310f84ce43",
+        "b30730a0eemshc261b3851f67015p123219jsnca66e69443cf",
+        "32d3f8856emshec3a7f9d298ad39p1594a7jsnd063ba87a615",
+        "3a3054add1mshaa261180f949236p1059a1jsnc451fef505ce",
+        "0490cbd4famsha5f6c9eb3c5b0bep1d02edjsn9f1dee49dc09",
+        "0387b85c24msh9e6dd27318a9b41p11f93ejsn45c86a31edc5",
+        "0cbd83e564msh837c6ee47e07343p19551cjsn7cafb1080993",
+        "bcac102b38msh3b43e6b9039840fp1a8bdcjsn03aa36bb3eb1"
+    )
+}
 }
--- app/src/main/java/com/fdev/lay/ui/base/MainActivity.java
+++ app/src/main/java/com/fdev/lay/ui/base/MainActivity.java
@@ -1,15 +1,15 @@
 package com.fdev.lay.ui.base;
 
-import androidx.annotation.RequiresApi;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.widget.TextView;
 
 import com.fdev.lay.R;
+import com.fdev.lay.common.LayConstant;
 import com.fdev.lay.data.local.database.Account;
 import com.fdev.lay.data.local.database.DatabaseHandler;
 import com.fdev.lay.presentation.ui.login.LoginActivity;
@@ -23,117 +23,90 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Timer timer;
-    TextView welcomeText;
-    private boolean firsttime;
-    private DatabaseHandler db = new DatabaseHandler(this);
-    private static String APItoken;
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-
+    private boolean firstTime;
+    private DatabaseHandler dbHandler = new DatabaseHandler(this);
+    private static String apiToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setup();
+    }
 
-        String[] key = new String[]{
-                "faf5f15f11mshde808885082bbb3p1b4a6ejsn3384755128b8",
-                "1c94f2813amshf75073ced6bbcdep11f75ajsnf0310f84ce43",
-                "b30730a0eemshc261b3851f67015p123219jsnca66e69443cf",
-                "32d3f8856emshec3a7f9d298ad39p1594a7jsnd063ba87a615",
-                "3a3054add1mshaa261180f949236p1059a1jsnc451fef505ce",
-                "0490cbd4famsha5f6c9eb3c5b0bep1d02edjsn9f1dee49dc09",
-                "0387b85c24msh9e6dd27318a9b41p11f93ejsn45c86a31edc5",
-                "0cbd83e564msh837c6ee47e07343p19551cjsn7cafb1080993",
-                "bcac102b38msh3b43e6b9039840fp1a8bdcjsn03aa36bb3eb1"
-        };
-
-        int random = Math.abs(ThreadLocalRandom.current().nextInt() % 10);
-
-        APItoken = key[random];
-
+    private void setup() {
+        setAPIKey();
+        strictMode();
         userStatus();
+        welcome();
+        redirect();
+    }
+
+    private void setAPIKey() {
+        int random = Math.abs(ThreadLocalRandom.current().nextInt() % 10);
+        apiToken = LayConstant.Constants.INSTANCE.getKey().get(random);
+    }
+
+    private void strictMode() {
         StrictMode.ThreadPolicy policy = new
                 StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        welcome();
-        int delay;
-        if (db.getIsLiteMode()) delay = 0;
-        else delay = 1200;
-        timer = new Timer();
-
-        timer.schedule(new TimerTask() {
-                           @Override
-                           public void run() {
-                               Intent intent;
-
-                               if (firsttime)
-                                   intent = new Intent(MainActivity.this, FirstTime2.class);
-                               else if (db.getIsLoggedIn())   // User has logged in
-                                   intent = new Intent(MainActivity.this, HomePage.class);
-                               else  // User hasn't logged in
-                                   intent = new Intent(MainActivity.this, LoginActivity.class);
-
-                               startActivity(intent);
-                               finish();
-
-                           }
-                       }
-                , delay
-        );
-
-
     }
-
 
     private void userStatus() {
+        //If there is no data created in account table it's user's first time and it creates the account table in sqlite
+        if (dbHandler.getaccountsCount() == 0) {
+            dbHandler.addaccount(new Account(1, 1, 0, 0, 1));
+            firstTime = true;
 
-
-        if (db.getaccountsCount() == 0) {
-            //It's there is no data created in account table it's first time and it creates the account table in sqlite
-            db.addaccount(new Account(1, 1, 0, 0, 1));
-            firsttime = true;
-
-        } else if (db.getaccountsCount() >= 1) {
-            // to delete extra accounts in case if there's more than one
-            for (int i = 0; i > db.getaccountsCount(); i++)
+        } else if (dbHandler.getaccountsCount() >= 1) { // to delete extra accounts in case if there's more than one
+            for (int i = 0; i > dbHandler.getaccountsCount(); i++)
                 try {
-                    db.deleteaccount(new Account(i, 0, 0, 0, 1));
+                    dbHandler.deleteaccount(new Account(i, 0, 0, 0, 1));
                 } catch (Exception e) {
-                    db.deleteaccount(new Account(i, 1, 0, 0, 1));
+                    dbHandler.deleteaccount(new Account(i, 1, 0, 0, 1));
                 }
 
-        } else if (db.getIsFirtsTime()) firsttime = true;
-        else firsttime = false;
+        } else if (dbHandler.getIsFirtsTime()) firstTime = true;
+        else firstTime = false;
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     private void welcome() {
 
         Calendar rightNow = Calendar.getInstance();
         int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+        TextView welcomeText = findViewById(R.id.creativeText);
+        String WelcomeText;
 
-        String time = String.valueOf(hour);
-        welcomeText = findViewById(R.id.creativeText);
-        String WelcomeText = null;
-
-        if (hour < 12 && hour > 5) {
-            WelcomeText = "Good Morning";
-        } else if (hour > 12 && hour < 18) {
-            WelcomeText = "Good Afternoon";
-        } else {
-            WelcomeText = "Good Evening";
-        }
+        if (hour < 12 && hour > 5) WelcomeText = "Good Morning";
+        else if (hour > 12 && hour < 18) WelcomeText = "Good Afternoon";
+        else WelcomeText = "Good Evening";
 
         welcomeText.setText(WelcomeText);
+    }
 
+    private void redirect() {
+        int delay;
+        if (dbHandler.getIsLiteMode()) delay = 0;
+        else delay = 1200;
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+                           @Override
+                           public void run() {
+                               Intent intent;
+                               if (firstTime) intent = new Intent(MainActivity.this, FirstTime2.class);
+                               else if (dbHandler.getIsLoggedIn()) intent = new Intent(MainActivity.this, HomePage.class);
+                               else intent = new Intent(MainActivity.this, LoginActivity.class);
+                               startActivity(intent);
+                               finish();
+                           }
+                       }
+                , delay
+        );
     }
 
     public static String getToken() {
-        return APItoken;
+        return apiToken;
     }
-
 }
